7c0256d80967cd5469d299c8c02bd8e9fff07821
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2025-06-12 16:41:04 +0100

    Add the production database url

--- src/config.js
+++ src/config.js
@@ -1,12 +1,14 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const config = {
-    development: {
-        databaseUrl: process.env.DATABASE_URL
-    }
+  development: {
+    databaseUrl: process.env.DATABASE_URL_DEV,
+  },
+  production: {
+    databaseUrl: process.env.DATABASE_URL,
+  },
+};
 
-}
+const environment = process.env.NODE_ENV || "development";
 
-const environment = process.env.NODE_ENV || 'development'
-
-module.exports = config[environment]
+module.exports = config[environment];

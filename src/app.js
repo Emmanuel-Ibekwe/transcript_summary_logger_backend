c731b47c78153725301e5b9d72834ff3c7407c01
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2025-06-19 13:36:41 +0100

    chore: update frontend development url

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.use(compression());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
   })
 );
 

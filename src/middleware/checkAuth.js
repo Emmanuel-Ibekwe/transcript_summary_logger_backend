e65e0f3ba15ea5345c374f93f64d503e2744d427
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2025-06-23 05:22:43 +0100

    chore: update error message

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -1,32 +1,32 @@
 const dotenv = require("dotenv");
-const createHttpError = require('http-errors');
+const createHttpError = require("http-errors");
 const jwt = require("jsonwebtoken");
 
-const checkAuth = async(req, res, next) => {
-    try {
-        const authHeader = req.get("Authorization");
-        if(!authHeader) {
-            return next(createHttpError.Unauthorized("Not authorized."))
-        }
-
-        const splitArray = authHeader.split(" ");
-        const lastIndex = splitArray.length - 1;
-        const token = authHeader.split(" ")[lastIndex];
-
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
-            if(err) {
-                return next(createHttpError.Unauthorized("Not authorized."));
-            }
-
-            req.user = payload;
-        });
-        next();
-    } catch(error) {
-        if(!error.status) {
-            error.status = 500;
-        }
-        next(error);
+const checkAuth = async (req, res, next) => {
+  try {
+    const authHeader = req.get("Authorization");
+    if (!authHeader) {
+      return next(createHttpError.Unauthorized("User not authorized."));
     }
-}
+
+    const splitArray = authHeader.split(" ");
+    const lastIndex = splitArray.length - 1;
+    const token = authHeader.split(" ")[lastIndex];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+      if (err) {
+        return next(createHttpError.Unauthorized("User not authorized."));
+      }
+
+      req.user = payload;
+    });
+    next();
+  } catch (error) {
+    if (!error.status) {
+      error.status = 500;
+    }
+    next(error);
+  }
+};
 
 module.exports = checkAuth;
